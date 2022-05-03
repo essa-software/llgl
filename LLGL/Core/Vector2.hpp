9aed02d82f662fbd395356f01ab208ae481841e5
Maciej <sppmacd@pm.me>
2022-05-03 13:58:25 +0200

    Core: Static_cast components in converting constructor

--- LLGL/Core/Vector2.hpp
+++ LLGL/Core/Vector2.hpp
@@ -13,19 +13,26 @@ struct UninitializedVector2
     UninitializedVector2() = default;
 
     UninitializedVector2(T x_, T y_)
-    : x{x_}, y{y_} {}
+        : x { x_ }
+        , y { y_ }
+    {
+    }
 
     explicit UninitializedVector2(concepts::Vector2 auto other)
-    : x{other.x}, y{other.y} {}
+        : x { other.x }
+        , y { other.y }
+    {
+    }
 
     UninitializedVector2& operator=(concepts::Vector2 auto other)
     {
-        if constexpr(std::is_same_v<decltype(&other), decltype(this)>)
+        if constexpr (std::is_same_v<decltype(&other), decltype(this)>)
         {
-            if(this == &other)
+            if (this == &other)
                 return *this;
         }
-        x = other.x; y = other.y;
+        x = other.x;
+        y = other.y;
         return *this;
     }
 
@@ -39,19 +46,26 @@ public:
     Vector2() = default;
 
     Vector2(T x_, T y_)
-    : x{x_}, y{y_} {}
+        : x { x_ }
+        , y { y_ }
+    {
+    }
 
     explicit Vector2(concepts::Vector2 auto other)
-    : x{other.x}, y{other.y} {}
+        : x { static_cast<T>(other.x) }
+        , y { static_cast<T>(other.y) }
+    {
+    }
 
     Vector2& operator=(concepts::Vector2 auto other)
     {
-        if constexpr(std::is_same_v<decltype(&other), decltype(this)>)
+        if constexpr (std::is_same_v<decltype(&other), decltype(this)>)
         {
-            if(this == &other)
+            if (this == &other)
                 return *this;
         }
-        x = other.x; y = other.y;
+        x = other.x;
+        y = other.y;
         return *this;
     }
 
