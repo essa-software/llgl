543cc4bcf4ccae3dc69bb57fe7c278457222544b
Maciej <sppmacd@pm.me>
2022-05-28 21:02:25 +0200

    Renderer: Use given view matrix instead of the command one

--- LLGL/Renderer/BatchRenderer.cpp
+++ LLGL/Renderer/BatchRenderer.cpp
@@ -26,11 +26,14 @@ void BatchRenderer::end_draw()
     m_vertexes.clear();
 }
 
-void BatchRenderer::render(Renderer& renderer, DrawState) const
+void BatchRenderer::render(Renderer& renderer, DrawState state) const
 {
-    // TODO: Use given RendererConfig somehow (combine transforms etc).
     for (auto const& command : m_commands)
-        renderer.draw_vao(command.vao, command.primitive_type, command.state);
+    {
+        auto this_state = command.state;
+        this_state.view_matrix = state.view_matrix;
+        renderer.draw_vao(command.vao, command.primitive_type, this_state);
+    }
 }
 
 }
