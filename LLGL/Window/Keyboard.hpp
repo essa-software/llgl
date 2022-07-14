167afd838696fd2f22ecf687b6c74ef527a40e17
Maciej <sppmacd@pm.me>
2022-07-14 15:59:27 +0200

    Window: Add more keycodes

--- LLGL/Window/Keyboard.hpp
+++ LLGL/Window/Keyboard.hpp
@@ -4,18 +4,44 @@
 
 #include <SDL2/SDL_keyboard.h>
 
-namespace llgl
-{
+namespace llgl {
 
 #define ENUMERATE_KEYBOARD_KEYCODES(K) \
     K(A, SDLK_a)                       \
+    K(B, SDLK_b)                       \
+    K(C, SDLK_c)                       \
     K(D, SDLK_d)                       \
+    K(E, SDLK_e)                       \
+    K(F, SDLK_f)                       \
+    K(G, SDLK_g)                       \
+    K(H, SDLK_h)                       \
+    K(I, SDLK_i)                       \
+    K(J, SDLK_j)                       \
+    K(K, SDLK_k)                       \
+    K(L, SDLK_l)                       \
+    K(M, SDLK_m)                       \
+    K(N, SDLK_n)                       \
+    K(O, SDLK_o)                       \
+    K(P, SDLK_p)                       \
+    K(Q, SDLK_q)                       \
+    K(R, SDLK_r)                       \
     K(S, SDLK_s)                       \
+    K(T, SDLK_t)                       \
+    K(U, SDLK_u)                       \
+    K(V, SDLK_v)                       \
     K(W, SDLK_w)                       \
-    K(Space, SDLK_SPACE)
+    K(X, SDLK_x)                       \
+    K(Y, SDLK_y)                       \
+    K(Z, SDLK_z)                       \
+    K(Up, SDLK_UP)                     \
+    K(Down, SDLK_DOWN)                 \
+    K(Left, SDLK_LEFT)                 \
+    K(Right, SDLK_RIGHT)               \
+    K(Enter, SDLK_RETURN)              \
+    K(Space, SDLK_SPACE)               \
+    K(Tab, SDLK_TAB)
 
-enum class KeyCode
-{
+enum class KeyCode {
 #define __ENUMERATE_KEY(key, value) key = value,
     ENUMERATE_KEYBOARD_KEYCODES(__ENUMERATE_KEY)
 #undef __ENUMERATE_KEY
@@ -23,8 +49,7 @@ enum class KeyCode
 
 constexpr std::string_view to_string(KeyCode key)
 {
-    switch (key)
-    {
+    switch (key) {
 #define __ENUMERATE_KEY(key, value) \
     case KeyCode::key:              \
         return #key;
