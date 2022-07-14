7f443bf98ff2f59b479c7edcbb70fa64f9553dc0
Maciej <sppmacd@pm.me>
2022-07-14 15:59:17 +0200

    OpenGL: Add LineStrip primitive type

--- LLGL/OpenGL/Vertex.hpp
+++ LLGL/OpenGL/Vertex.hpp
@@ -13,6 +13,7 @@ enum class PrimitiveType {
     Invalid = -1,
     Points = GL_POINTS,
     Lines = GL_LINES,
+    LineStrip = GL_LINE_STRIP,
     Triangles = GL_TRIANGLES,
     TriangleStrip = GL_TRIANGLE_STRIP,
     TriangleFan = GL_TRIANGLE_FAN,
