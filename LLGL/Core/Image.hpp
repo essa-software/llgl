7480b63bb0268cb2d2c8a3de1a25776b427e91cc
Maciej <sppmacd@pm.me>
2022-07-18 13:18:27 +0200

    Core: Add ALWAYS_INLINE to get_pixel, set_pixel, pixel_index

--- LLGL/Core/Image.hpp
+++ LLGL/Core/Image.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <EssaUtil/Color.hpp>
+#include <EssaUtil/Config.hpp>
 #include <EssaUtil/Vector.hpp>
 #include <span>
 
@@ -20,8 +21,8 @@ public:
     std::span<Util::Color const> pixels() const { return { m_pixels, pixel_count() }; }
     std::span<Util::Color> pixels() { return { m_pixels, pixel_count() }; }
 
-    Util::Color get_pixel(Util::Vector2u pos) const { return m_pixels[pixel_index(pos)]; }
-    void set_pixel(Util::Vector2u pos, Util::Color color) { m_pixels[pixel_index(pos)] = color; }
+    ESSA_ALWAYS_INLINE Util::Color get_pixel(Util::Vector2u pos) const { return m_pixels[pixel_index(pos)]; }
+    ESSA_ALWAYS_INLINE void set_pixel(Util::Vector2u pos, Util::Color color) { m_pixels[pixel_index(pos)] = color; }
 
     void insert_image_in_bounds(Image const& other, Util::Vector2u where);
 
@@ -31,7 +32,7 @@ public:
 private:
     Image(Util::Vector2u size);
 
-    size_t pixel_index(Util::Vector2u pos) const { return pos.x() + pos.y() * m_size.x(); }
+    ESSA_ALWAYS_INLINE size_t pixel_index(Util::Vector2u pos) const { return pos.x() + pos.y() * m_size.x(); }
 
     Util::Color* m_pixels = nullptr;
     Util::Vector2u m_size;
